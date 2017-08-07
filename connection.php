0f7b6ab9059b7a72efc42e1f85806f887ffca0b6
Salim <salimsalaues@gmail.com>
2017-08-06 21:46:55 -0700

    new connection

---
+++ connection.php
@@ -0,0 +1,3 @@
+<?php
+	$link = mysqli_connect("localhost","tinyRick","Wubalubadubdub!","tinyRick") or die("Error " . mysqli_error($link)); 
+?>
--- index.php
+++ index.php
@@ -1,16 +1,6 @@
 <?php 
     session_start(); 
-    require("config.php"); 
-    if(isset($_GET['page']))
-    {       
-        $pages=array("products", "cart");       
-        if(in_array($_GET['page'], $pages))              
-            $_page=$_GET['page'];
-        else  
-            $_page="products"; 
-    }
-    else          
-        $_page="products"; 
+    require("connection.php"); 
 ?> 
 <!DOCTYPE html>
 <html lang="en">
