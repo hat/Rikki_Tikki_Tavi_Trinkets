18452809dc6da40e06fc0c5e6906f86c70dcfc9a
Salim <salimsalaues@gmail.com>
2017-08-06 21:16:52 -0700

    database connection

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-   define('DB_SERVER', 'localhost:8888');
+   define('DB_SERVER', 'localhost');
    define('DB_USERNAME', 'tinyRick');
    define('DB_PASSWORD', 'Wubalubadubdub!');
    define('DB_DATABASE', 'tinyRick');
@@ -7,9 +7,6 @@
    $conn = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD);
     if (!$conn)
         die("Connection Failed: " . mysql_connect_error());
-//    $sql = "CREATE DATABASE tinyRick";
-//    if (mysql_query($conn, $sql))
-//        echo "Database Created Successfully";
     if (!mysql_select_db(DB_DATABASE))
         echo "Error creating database: " . mysql_error($conn);
     
