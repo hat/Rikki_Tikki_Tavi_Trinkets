063dc174f60b7fec4eae3d6bcb4ef210cd221fb2
Tony Hendrick <thendrick@Tony-Hendricks-MacBook-Pro.local>
2017-08-07 00:29:32 -0700

    Conection

--- connection.php
+++ connection.php
@@ -1,3 +1,14 @@
 <?php
-	$link = mysqli_connect("localhost","tinyRick","Wubalubadubdub!","tinyRick") or die("Error " . mysqli_error($link)); 
+
+session_start();
+$host="localhost";
+$user="tinyRick";
+$pass="Wubalubadubdub!";
+$database="tinyRick";
+$link=mysqli_connect($host,$user,$pass);
+$sql="CREATE DATABASE tinyRick";
+$req=mysqli_query($link, $sql);
+mysqli_select_db($link, $database);
+
+//	$link = mysqli_connect("localhost","tinyRick","Wubalubadubdub!","tinyRick") or die("Error " . mysqli_error($link)); 
 ?>
